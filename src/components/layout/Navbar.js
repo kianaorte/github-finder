67cb57d0d82aea245dea4287c11c014c7aafb48e
kianaorte <50307803+kianaorte@users.noreply.github.com>
2021-03-13 21:55:21 +1300

    Connected to api and added a loading spinner

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-    static defaultProps ={
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    };
+const Navbar = ({ icon, title }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github',
+};
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
-    render() {
-        return (
-            <div className="navbar bg-primary">
-                <h1>
-                    <i className={this.props.icon}/> {this.props.title}
-                </h1>
-            </div>
-        )
-    }
-}
-
-export default Navbar
+export default Navbar;
