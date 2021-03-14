7d9c1567cdc0bf58d39bcb616747452fd1f8a4de
kianaorte <50307803+kianaorte@users.noreply.github.com>
2021-03-15 10:02:49 +1300

    Added a clearUsers to clear the state after search

--- src/App.js
+++ src/App.js
@@ -10,17 +10,10 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   this.setState({ users: res.data.items, loading: false });
-  // }
 
+  // Search users in the directory
   searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=
       ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
@@ -29,12 +22,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //clears out the state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
